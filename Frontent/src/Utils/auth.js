72b495c059ca52fb68bdc3f44a4ae371986586a2
Nghia Doan <3120221424@ued.udn.vn>
2024-06-10 23:10:22 +0700

    Add files via upload

---
+++ Frontent/src/Utils/auth.js
@@ -0,0 +1,8 @@
+export function isAuth() {
+  const userData = localStorage.getItem("user-data");
+
+  if (userData == null || userData == undefined || userData === "") {
+    return false;
+  }
+  return true;
+}
