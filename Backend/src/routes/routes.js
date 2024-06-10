3a292db16b490be7adf0a711b454ce2af6cb1b6a
Nghia Doan <3120221424@ued.udn.vn>
2024-06-10 23:27:28 +0700

    Update routes.js

--- Backend/src/routes/routes.js
+++ Backend/src/routes/routes.js
@@ -1,31 +1,35 @@
 const express = require("express");
-const collection = require("../controllers/collection_note")
-const userLogin = require("../controllers/login")
-const userSignup = require("../controllers/signup")
-const user = require("../controllers/logout")
-const auth =require('../controllers/middlewares/aceessToken')
+const collection = require("../controllers/collection_note");
+const userLogin = require("../controllers/login");
+const notes = require('../controllers/notes');
+const userSignup = require("../controllers/signup");
+const userLogout = require("../controllers/logout");
+const auth =require('../middlewares/auth')
  const cors = require('cors');
  const routes = express.Router();
 
 routes.use(cors());
 routes.get('/auth', auth.authenticateToken, (req, res)=>{
-    res.json({ user, token });
-
+ res.json();
 });
 
-routes.get('/Users/signup',(req, res)=>{
-    res.send("");
-})
- routes.post('/Users/signup', userSignup.signupUser);
 
-routes.get("/Users/login",userLogin.getLogin);
+
+routes.post('/Users/signup', userSignup.signupUser);
+//Login-Logout
 routes.post("/Users/login", userLogin.authLogin);
-
+routes.delete ("Users/logout", userLogout.LogoutUser)
 // routes.post("/Notes/new", auth.newNote);
 // routes.get("/Notes/new", auth.getNotes);
+routes.post('/Users/logout',auth.authenticateToken ,userLogout.LogoutUser);
+routes.post("/Collection/new", auth.authenticateToken, collection.NewCollection);
+routes.get("/Collection/display/", collection.GetCollection);
+//CRUD routes notes
+routes.post("/Notes/new", auth.authenticateToken, notes.CreateNotes);
+routes.get("/Notes/show", notes.ReadNotes );
+routes.post('/Notes/delete',notes.DeleteNotes);
+routes.post("/Notes/update", notes.UpdatesNotes);
 
-routes.post("/Collection/new",collection.NewCollection);
-// routes.get("/Collection/note",auth.authenticateToken, collection.GetCollection)
 // ;
-routes.post("User/logout", user.ClearCookie);
+// routes.post("User/logout", user.ClearCookie);
 module.exports =routes;
